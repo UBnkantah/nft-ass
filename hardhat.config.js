6dfc68caeec803c45c4f9f0b42c6f4736d01752e
UBnkantah <ubongnkantahuwem@gmail>
2023-09-06 03:27:40 -0700

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-ethers')
+require('dotenv').config()
+require('@openzeppelin/hardhat-upgrades');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
 };
